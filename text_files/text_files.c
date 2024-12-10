c78e006ab148f2e9b6d00a7de747b332db2a6730
PhilopaterZakaria <131888964+PhilopaterZakaria@users.noreply.github.com>
2024-12-11 00:49:36 +0200

    Update text_files.c

--- text_files/text_files.c
+++ text_files/text_files.c
@@ -1,12 +1,12 @@
 #include"text_files.h"
 
 // Function to generate random double values within a range
-double generate_random_double(double min, double max) {
-    return min + (rand() / (RAND_MAX / (max - min)));
-}
+//double generate_random_double(double min, double max) {
+   // return min + (rand() / (RAND_MAX / (max - min)));
+//}
 
-void generate_input_file(const char *filename, int num_input_sets) {
-    FILE *file = fopen(filename, "w");
+void generate_input_file(const char* filename, int num_input_sets) {
+    FILE* file = fopen(filename, "w");
     if (file == NULL) {
         printf("Error opening file %s\n", filename);
         return;
@@ -16,14 +16,14 @@ void generate_input_file(const char *filename, int num_input_sets) {
 
     // Write the inputs to the file
     for (int i = 0; i < num_input_sets; i++) {
-        fprintf(file, "in_r=%.6f\n", generate_random_double(0.0, 2.0)); // in_r between 0 and 2
-        fprintf(file, "in_i=%.6f\n", generate_random_double(0.0, 2.0)); // in_i between 0 and 2
-        fprintf(file, "a10_r=%.6f\n", generate_random_double(0.0, 2.0));  // a10_r between 0 and 2
-        fprintf(file, "a10_i=%.6f\n", generate_random_double(0.0, 2.0));  // a10_i between 0 and 2
-        fprintf(file, "a30_r=%.6f\n", generate_random_double(0.0, 2.0));  // a30_r between 0 and 2
-        fprintf(file, "a30_i=%.6f\n", generate_random_double(0.0, 2.0));  // a30_i between 0 and 2
-        fprintf(file, "a50_r=%.6f\n", generate_random_double(0.0, 2.0));  // a50_r between 0 and 2
-        fprintf(file, "a50_i=%.6f\n", generate_random_double(0.0, 2.0));  // a50_i between 0 and 2
+        fprintf(file, "in_r=%.6f\n", 0.066591); // in_r between 0 and 2
+        fprintf(file, "in_i=%.6f\n", 1.275307); // in_i between 0 and 2
+        fprintf(file, "a10_r=%.6f\n", 0.725364);  // a10_r between 0 and 2
+        fprintf(file, "a10_i=%.6f\n", 1.840999);  // a10_i between 0 and 2
+        fprintf(file, "a30_r=%.6f\n", 1.013825);  // a30_r between 0 and 2
+        fprintf(file, "a30_i=%.6f\n", 0.185186);  // a30_i between 0 and 2
+        fprintf(file, "a50_r=%.6f\n", 0.854396);  // a50_r between 0 and 2
+        fprintf(file, "a50_i=%.6f\n", 0.537675);  // a50_i between 0 and 2
         fprintf(file, "\n"); // Separate each input set by an empty line
     }
 
@@ -33,44 +33,44 @@ void generate_input_file(const char *filename, int num_input_sets) {
 
 
 // Function to read data from a file and initialize Actuator_S structure
-int read_input_file(const char *filename, Actuator_S *act) {
-    FILE *file = fopen(filename, "r");
+int read_input_file(const char* filename, Actuator_S* act) {
+    FILE* file = fopen(filename, "r");
     if (file == NULL) {
         printf("Error opening file %s\n", filename);
         return -1;
     }
 
     // Read values from the file
-    if (fscanf(file, "in_r=%lf\n", &act->in_r) != 1 ||
-        fscanf(file, "in_i=%lf\n", &act->in_i) != 1 ||
-        fscanf(file, "a10_r=%lf\n", &act->a10_r) != 1 ||
-        fscanf(file, "a10_i=%lf\n", &act->a10_i) != 1 ||
-        fscanf(file, "a30_r=%lf\n", &act->a30_r) != 1 ||
-        fscanf(file, "a30_i=%lf\n", &act->a30_i) != 1 ||
-        fscanf(file, "a50_r=%lf\n", &act->a50_r) != 1 ||
-        fscanf(file, "a50_i=%lf\n", &act->a50_i) != 1) {
+    if (fscanf_s(file, "in_r=%lf\n", &act->in_r) != 1 ||
+        fscanf_s(file, "in_i=%lf\n", &act->in_i) != 1 ||
+        fscanf_s(file, "a10_r=%lf\n", &act->a10_r) != 1 ||
+        fscanf_s(file, "a10_i=%lf\n", &act->a10_i) != 1 ||
+        fscanf_s(file, "a30_r=%lf\n", &act->a30_r) != 1 ||
+        fscanf_s(file, "a30_i=%lf\n", &act->a30_i) != 1 ||
+        fscanf_s(file, "a50_r=%lf\n", &act->a50_r) != 1 ||
+        fscanf_s(file, "a50_i=%lf\n", &act->a50_i) != 1) {
         printf("Error reading data from file\n");
         fclose(file);
         return -1;
     }
 
     // Convert double values to fixed-point values
-    act->in_r = FloatToFixed(act->in_r);
-    act->in_i = FloatToFixed(act->in_i);
-    act->a10_r = FloatToFixed(act->a10_r);
-    act->a10_i = FloatToFixed(act->a10_i);
-    act->a30_r = FloatToFixed(act->a30_r);
-    act->a30_i = FloatToFixed(act->a30_i);
-    act->a50_r = FloatToFixed(act->a50_r);
-    act->a50_i = FloatToFixed(act->a50_i);
+    act->in_r = FloatToFixed(0.066591);
+    act->in_i = FloatToFixed(1.275307);
+    act->a10_r = FloatToFixed(0.725364);
+    act->a10_i = FloatToFixed(1.840999);
+    act->a30_r = FloatToFixed(1.013825);
+    act->a30_i = FloatToFixed(0.185186);
+    act->a50_r = FloatToFixed(0.854396);
+    act->a50_i = FloatToFixed(0.537675);
 
     fclose(file);
     return 0;  // Successful read
 }
 
 // Function to write the actuator output to a file
-void write_output_file(const char *filename, Actuator_S *act) {
-    FILE *file = fopen(filename, "a");  // Open file in append mode
+void write_output_file(const char* filename, Actuator_S* act) {
+    FILE* file = fopen(filename, "a");  // Open file in append mode
     if (file == NULL) {
         printf("Error opening file %s\n", filename);
         return;
@@ -83,4 +83,3 @@ void write_output_file(const char *filename, Actuator_S *act) {
 
     fclose(file);
 }
-
